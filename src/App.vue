edb5815ff79a3d8dad07664c5dba1e439c060cc5
diprojose <parejojose77@gmail.com>
2022-08-14 22:22:15 -0500

    First configuration

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Hero from './components/Hero/Hero.vue';
+import Header from "./components/Header/Header.vue";
+
+export default {
+  components: {
+    Hero,
+    Header
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Hero />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header-container">
+    <p>Header section</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
