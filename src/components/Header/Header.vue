9338d2976c10a980ff8134e3ed2e6916603048dd
diprojose <parejojose77@gmail.com>
2022-09-19 18:38:14 -0500

    Header component in desktop and responsive

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,129 @@
-<script setup lang="ts">
-
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+      showMenu: false,
+    }
+  },
+  methods: {
+    openResponsiveMenu(): void {
+      this.showMenu = !this.showMenu;
+    }
+  }
+}
 </script>
 
 <template>
   <div class="header-container">
-    <p>Header section</p>
+    <div class="logo">
+      <h3 class="logo-jp">JP<span class="logo-dot">.</span></h3>
+    </div>
+    <div class="menu">
+      <ul class="menu-list" :class="{ responsive: showMenu }">
+        <img @click="openResponsiveMenu()" src="../../assets/close.png" class="close-button" alt="close">
+        <li class="menu-list-item">About</li>
+        <li class="menu-list-item">Work</li>
+        <li class="menu-list-item">Experience</li>
+        <li class="menu-list-item">Contact</li>
+        <li class="menu-list-item">
+          <button class="portfolio-button">Resume</button>
+        </li>
+      </ul>
+      <div @click="openResponsiveMenu()" :class="{ responsive: showMenu }" class="overlay"></div>
+      <img @click="openResponsiveMenu()" class="responsive-menu" src="../../assets/menu.png" alt="Menu">
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.header-container {
+  padding: 25px;
+  display: flex;
+  justify-content: space-between;
+  background-color: $dark-gray;
+  align-items: center;
+  .logo {
+    .logo-jp {
+      color: $white;
+      font-size: 30px;
+      margin: 0;
+      .logo-dot {
+        color: $green;
+      }
+    }
+  }
+  .menu {
+    .menu-list {
+      display: flex;
+      margin: 0;
+      align-items: center;
+      @include responsive(xs) {
+        display: none;
+      }
+      &.responsive {
+        width: 30%;
+        height: 100vh;
+        display: block;
+        position: fixed;
+        right: 0;
+        top: 0;
+        background-color: $medium-gray;
+        z-index: 2;
+        padding-left: 20px;
+        .close-button {
+          display: block;
+          padding: 20px 0;
+          cursor: pointer;
+        }
+        .menu-list-item {
+          padding-bottom: 20px;
+          text-align: right;
+          &:first-child {
+            padding-top: 20px;
+          }
+          &:last-child {
+            padding-right: 20px;
+          }
+        }
+      }
+      .close-button {
+        display: none;
+      }
+      .menu-list-item {
+        list-style-type: none;
+        padding-right: 30px;
+        color: $gray;
+        cursor: pointer;
+        &:hover {
+          color: $white;
+        }
+        &:last-child {
+          padding-right: 0;
+        }
+      }
+    }
+    .overlay {
+      display: none;
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100vh;
+      background-color: $black;
+      opacity: 0.5;
+      z-index: 1;
+      &.responsive {
+        display: block;
+      }
+    }
+    .responsive-menu {
+      display: none;
+      height: 30px;
+      cursor: pointer;
+      @include responsive(xs) {
+        display: block;
+      }
+    }
+  }
+}
 </style>
--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -3,57 +3,25 @@
 export default {
   data: () => {
     return {
-      countX: 0,
-      countY: 0,
-      directionX: '',
-      directionY: '',
-      movementX: 0,
-      movementY: 0,
-      multiplier: 0.2,
     }
   },
 
   methods: {
-    moveBlobs(event: any) {
-      console.log(event);
-      if (event.pageX !== this.countX) {
-        this.directionX = event.pageX < this.countX ? 'right' : 'left';
-        this.movementX = event.pageX - this.countX;
-        this.countX = event.pageX;
-      }
-      if (event.pageY !== this.countY) {
-        this.directionY = event.pageY > this.countY ? 'up' : 'down';
-        this.movementY = event.pageY - this.countY;
-        this.countY = event.pageY;
-      }
-    }
   },
 
   computed: {
-    movementStyles() {
-      return {
-        transform: `translate(${(this.movementX * this.multiplier)}px, ${(this.movementY * this.multiplier)}px)`
-      }
-    }
   }
 }
 
 </script>
 
 <template>
-  <div class="hero-container" @mousemove="moveBlobs($event)">
+  <div class="hero-container">
     <div class="hero-text-container">
-      <h1>José Antonio Parejo Tovar</h1>
-      <h2>Senior Software Engineer {{ count }}</h2>
+      <h2 class="name">José Antonio Parejo Tovar</h2>
+      <h1 class="position">Senior Software Engineer</h1>
     </div>
     <div class="hero-image-container">
-      <svg
-        viewBox="0 0 200 200"
-        xmlns="http://www.w3.org/2000/svg"
-        :style="movementStyles">
-        <path fill="#06bcc1" d="M46.2,-50C58.8,-33.6,67.1,-16.8,65.9,-1.3C64.6,14.2,53.6,28.5,41.1,45C28.5,61.4,14.2,80.1,0.1,80C-14.1,80,-28.1,61.1,-44.5,44.6C-60.9,28.1,-79.6,14.1,-83.6,-4.1C-87.7,-22.2,-77.1,-44.3,-60.7,-60.7C-44.3,-77.1,-22.2,-87.8,-2.7,-85.1C16.8,-82.4,33.6,-66.4,46.2,-50Z" transform="translate(100 100)" />
-      </svg>
-      <img class="jose-photo" src="../../assets/jose.png" alt="">
     </div>
   </div>
 </template>
@@ -61,8 +29,8 @@ export default {
 <style lang="scss" scoped>
 
 .hero-container {
-  min-height: 600px;
-  background-color: $dark-blue;
+  min-height: 400px;
+  background-color: $dark-gray;
   display: grid;
   grid-template-columns: 1fr 1fr;
   padding: 25px;
@@ -78,15 +46,10 @@ export default {
   }
   .hero-image-container {
     position: relative;
-    svg {
-      width: 100%;
-      position: absolute;
-      top: 0;
-    }
     .jose-photo {
       width: auto;
       position: absolute;
-      max-height: 600px;
+      max-height: 400px;
     }
   }
 }
